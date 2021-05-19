baffda65513b3b9c648ea489c0a1685d39995d8b
Zeng Fung Liew <zengfung.liew@gmail.com>
2021-05-19 11:11:11 -0700

    added load image and extract channel functions

--- codes/sift.py
+++ codes/sift.py
@@ -3,21 +3,24 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 class SIFT_FeatureExtractor:
+    """
+    SIFT Feature Extractor class. This feature extraction technique goes through
+    the following steps:
+
+    1. Using SIFT to extract keypoints and descriptors of each image.
+    2. Using k-means to cluster the descriptors.
+    3. Construct "barplots" for each image on the number of descriptors found in each 
+    cluster. This step is also known as the Bag of Features (BoF) step.
+    4. The results are stored in a matrix and output as transformed data.
+
+    In the initialization step, we need to specify the following parameters:
+
+    - sift_nfeatures => default=0 (all features used)
+    - kmeans_nclusters => default=5
+    """
     def __init__(self, sift_nfeatures=0, kmeans_nclusters=5):
         """
-        SIFT Feature Extractor constructor. This feature extraction technique goes through
-        the following steps:
-
-        1. Using SIFT to extract keypoints and descriptors of each image.
-        2. Using k-means to cluster the descriptors.
-        3. Construct "barplots" for each image on the number of descriptors found in each 
-        cluster. This step is also known as the Bag of Features (BoF) step.
-        4. The results are stored in a matrix and output as transformed data.
-
-        In the initialization step, we need to specify the following parameters:
-
-        - sift_nfeatures => default=0 (all features used)
-        - kmeans_nclusters => default=5
+        Constructor for the SIFT Feature Extractor object.
         """
         self.sift_nfeatures = sift_nfeatures
         self.kmeans_nclusters = kmeans_nclusters
---
+++ codes/utils.py
@@ -0,0 +1,52 @@
+import cv2
+import pandas as pd
+import os
+
+def load_data():
+    """
+    Loads the train, validation, and test images in 3 separate lists along with their
+    respective labels.
+
+    Results are returned in the form of 3 tuples:
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test)
+    """
+    # get directory of current file
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    # switch working directory to data directory 
+    main_dir = os.path.dirname(file_dir)
+    os.chdir(main_dir + "\\data")
+    # import labels-files.csv as reference
+    df = pd.read_csv("labels-files.csv")
+    # initialize image and label lists, along with their start indices
+    X_train, y_train, idx_train = ([None]*65000, [None]*65000, 0)
+    X_valid, y_valid, idx_valid = ([None]*12500, [None]*12500, 0)
+    X_test, y_test, idx_test = ([None]*12500, [None]*12500, 0)
+    for i in range(90000):
+        if df.loc[i,"split"] == "train":
+            X_train[idx_train] = cv2.imread(df.loc[i,"path"])
+            y_train[idx_train] = df.loc[i,"label_idx"]
+            idx_train += 1
+        elif df.loc[i,"split"] == "valid":
+            X_valid[idx_valid] = cv2.imread(df.loc[i,"path"])
+            y_valid[idx_valid] = df.loc[i,"label_idx"]
+            idx_valid += 1
+        else:       # test data
+            X_test[idx_test] = cv2.imread(df.loc[i,"path"])
+            y_test[idx_test] = df.loc[i,"label_idx"]
+            idx_test += 1
+    return (X_train, y_train), (X_valid, y_valid), (X_test, y_test)
+
+def get_channel(X, channel):
+    """
+    Extracts the specific channel of each image in a given list.
+    
+    Inputs:
+    - X: list of images
+    - channel: channel to extract ("R", "G", or "B")
+    """
+    channel_map = {"R": 0, "G": 1, "B": 2}
+    Xt = [None]*len(X)
+    for (i, img) in enumerate(X):
+        assert img.ndim == 3, "Image does not have 3 channels"
+        Xt[i] = img[:,:,channel_map[channel]]
+    return Xt
